5847c89a47c93a38ef8aedb477e6733c3f9eef53
mariantune <mari.antunes911@gmail.com>
2023-07-11 09:11:30 -0300

    projeto finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -12,19 +12,40 @@ const peersonagens = document.querySelectorAll('.personagem');
 peersonagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter' , () => {
 
-        const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado');
+        if ('window.innerWidth < 450') {
+            window.scrollTo({top: 0, behavior: 'smooth' });
+        }
+
+        removerSelecaoDoPersonagem();
 
         personagem.classList.add('selecionado');
 
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+        alterarImagemPersonagemSelecionado(personagem);
 
-        const idPersonagem = personagem.attributes.id.value;
-        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
-
-        const nomePersonagem = document.getElementById('nome-personagem');
-        nomePersonagem.innerText = personagem.getAttribute ('data-name');
+        alterarNomePersonagemSelecionado(personagem);
         
-        
+        alterarDescricaoPersonagem(personagem);
     })
 })
+
+function alterarDescricaoPersonagem(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoDoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
